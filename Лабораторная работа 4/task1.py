ec1966dd1fa3694f1d2ade8abd0a090d0a294bb8
NeonilaPolevaya <154007798+NeonilaPolevaya@users.noreply.github.com>
2024-01-25 14:52:19 +0300

    Update task1.py

--- Лабораторная работа 4/task1.py
+++ Лабораторная работа 4/task1.py
@@ -8,11 +8,10 @@ filename = 'input.json'
 def task() -> float:
     with open(filename) as file:
         data = json.load(file)
-    value = 0
-    for dict in data:
-        value += dict["score"] * dict["weight"]
+    list_val = [dict["score"] * dict["weight"] for dict in data]
+    value = sum(list_val)
     return round(value, 3)
 
 
-
 print(task())
+
